cb37a5fdc35ce709f654b0211d72729a746746ec
Peeraphtw <judiciarypz@gmail.com>
2020-12-17 13:48:24 +0700

    unvar

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var req = new XMLHttpRequest();
 req.onreadystatechange = function (e) {
   if (req.readyState == req.DONE && req.status == 200) {
     data = JSON.parse(this.responseText);
-
-    const myRoot = document.querySelector('#root');
     data.map((item) => {
       document.querySelector('#root').innerHTML += `<tr><td>${item.id}</td>
         <td>${item.name}</td>
